a4dcd1521087b60eaa11ddab6c2cbcbd79506b6e
Emiliano1919 <emilianojg38@gmail.com>
2025-11-07 14:00:23 -0500

    Add benchmarking

---
+++ benchmark_test.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"testing"
+)
+
+func BenchmarkMonteCarloTreeSearc(b *testing.B) {
+	node := InitialRootNode()
+	for b.Loop() {
+		MonteCarloTreeSearch(node, 500, true)
+	}
+}
+
+func BenchmarkRollout(b *testing.B) {
+	node := InitialRootNode()
+	for b.Loop() {
+		SimulateRollout(node.GameState)
+	}
+}
+
+func BenchmarkRolloutParallel(b *testing.B) {
+	nodeP := InitialRootNode()
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			SimulateRollout(nodeP.GameState)
+		}
+	})
+}
+
+func BenchmarkMonteCarloTreeSearchParallel(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			nodePM := InitialRootNode()
+			MonteCarloTreeSearch(nodePM, 500, true)
+		}
+	})
+}
+
+func BenchmarkInitialNodeCreationParallel(b *testing.B) {
+	b.RunParallel(func(pb *testing.PB) {
+		for pb.Next() {
+			InitialRootNode()
+		}
+	})
+}
